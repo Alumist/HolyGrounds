1ef35b0423e23e11077cad14d7ce34e1f8816c83
Alumist <codealumist@gmail.com>
2024-05-14 14:33:39 -0700

    api/shops...

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import shops from './data/shops.js';
 
 const port = 5000;
 
@@ -6,6 +7,10 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('api running');
+});
+
+app.get('/api/shops', (req, res) => {
+  res.json(shops);
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
