ffe3d04edda4c853b1af73024d01c6973bfd538b
Alumist <codealumist@gmail.com>
2024-05-14 20:44:16 -0700

    backend configured & adding comments. Learn

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 import shops from './data/shops.js';
-
-const port = 5000;
+//to access the environment variable, we use process.env.PORT "process.env..."
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -13,4 +15,10 @@ app.get('/api/shops', (req, res) => {
   res.json(shops);
 });
 
+//:id is a placeholder for whatever id is injected into the url
+//using "find" to find the shop with the id that matches the id in the url, only if the shop id matches the id in the url, then return the shop.json
+app.get('/api/shops/:id', (req, res) => {
+  const shop = shops.find((s) => s._id === req.params.id);
+  res.json(shop);
+});
 app.listen(port, () => console.log(`Server running on port ${port}`));
