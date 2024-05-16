a326896d9f1810523deaf6740d8a91bef9db9f5d
Alumist <codealumist@gmail.com>
2024-05-16 00:13:36 -0700

    database/addingshops/current

--- backend/models/addedShopModel.js
+++ backend/models/addedShopModel.js
@@ -2,22 +2,17 @@ import mongoose from 'mongoose';
 
 const addedShopSchema = new mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
-    newShop: [
-      {
-        name: { type: String, required: true },
-        image: { type: String, required: true },
-        description: { type: String, required: true },
-        socials: { type: String, required: true },
-        location: { type: String, required: true },
-        address: { type: String, required: true },
-        shop: {
-          type: mongoose.Schema.Types.ObjectId,
-          required: true,
-          ref: 'Shop',
-        },
-      },
-    ],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    description: { type: String, required: true },
+    socials: { type: String, required: true },
+    location: { type: String, required: true },
+    address: { type: String, required: true },
   },
   {
     timestamps: true,
