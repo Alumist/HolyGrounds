647837af4c6ec4563674efb86c06fbafca3e2e70
Alumist <codealumist@gmail.com>
2024-05-16 17:51:42 -0700

    Revert "database/addingshops/current"
    This reverts commit a326896d9f1810523deaf6740d8a91bef9db9f5d.

--- backend/data/newShops.js
+++ backend/data/newShops.js
@@ -1,4 +1,4 @@
-const newShops = [
+const newShop = [
   {
     name: 'Twee',
     image:
@@ -9,16 +9,6 @@ const newShops = [
     location: 'Riverside, CA',
     address: '9344 Magnolia Ave, Riverside, CA 92503',
   },
-  {
-    name: 'New Shop',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2wWIVhiQhcCohxcboP-5QNY0z4qpjaK5NNjMKim_k0A&s',
-    description:
-      'Twee is a very small shop, and has beening growing from when it first started before covid. Althoug they have faced trials as a new buisness during covid, surely their coffee suficed.',
-    socials: 'https://www.instagram.com/tweecoffee/?hl=en',
-    location: 'Riverside, CA',
-    address: '9344 Magnolia Ave, Riverside, CA 92503',
-  },
 ];
 
-export default newShops;
+export default newShop;
