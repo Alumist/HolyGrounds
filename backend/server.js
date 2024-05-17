0b0d48554b83271a4e694d79d1690fc3c547564a
Alumist <codealumist@gmail.com>
2024-05-16 23:51:13 -0700

    backend with routes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDb from './config/db.js';
-import shops from './data/shops.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import shopRoutes from './routes/shopRoutes.js';
 //to access the environment variable, we use process.env.PORT "process.env..."
 const port = process.env.PORT || 5000;
 
@@ -14,14 +15,8 @@ app.get('/', (req, res) => {
   res.send('api running');
 });
 
-app.get('/api/shops', (req, res) => {
-  res.json(shops);
-});
+app.use('/api/shops', shopRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
-//:id is a placeholder for whatever id is injected into the url
-//using "find" to find the shop with the id that matches the id in the url, only if the shop id matches the id in the url, then return the shop.json
-app.get('/api/shops/:id', (req, res) => {
-  const shop = shops.find((s) => s._id === req.params.id);
-  res.json(shop);
-});
 app.listen(port, () => console.log(`Server running on port ${port}`));
