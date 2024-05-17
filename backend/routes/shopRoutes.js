f4555bd72920c635a2c3e89ac3b30e07fa27e4a4
Alumist <codealumist@gmail.com>
2024-05-16 17:53:31 -0700

    fix

--- backend/routes/shopRoutes.js
+++ backend/routes/shopRoutes.js
@@ -1,8 +1,18 @@
 import express from 'express';
 const router = express.Router();
+import shops from '../data/shops.js';
 import { getShops, getShopById } from '../controllers/shopController.js';
 
-router.route('/').get(getShops);
-router.route('/:id').get(getShopById);
+router.get('/', (req, res) => {
+    res.json(shops);
+});
+
+router.get('/:id', (req, res) => {
+    const shop = shops.find((shop) => shop._id === req.params.id);
+    res.json(shop);
+});
+
+// router.route('/').get(getShops);
+// router.route('/:id').get(getShopById);
 
 export default router;
